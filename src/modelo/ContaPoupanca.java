1089079854be9e54a3af03c4c5cde7b3d97ef882
CICERO <cicero.alves@ifpb.edu.br>
2025-07-31 16:39:42 -0300

    Aula 31/07/2025

---
+++ src/modelo/ContaPoupanca.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class ContaPoupanca extends Conta {
+    public ContaPoupanca(String nomeTitular, double saldo){
+        super(nomeTitular, saldo);
+    }
+
+    public void reajustar(double percentual) {
+        double rendimento = getSaldo() * percentual;
+        this.saldo += rendimento;
+    }
+}
