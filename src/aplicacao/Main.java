e9b82d0e218561dc5dc67f6a8b38bdeb588dc9ba
CICERO <cicero.alves@ifpb.edu.br>
2025-09-04 15:59:16 -0300

    Aula 04/09/2025

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,5 +1,6 @@
 package aplicacao;
 
+import modelo.Cliente;
 import modelo.Conta;
 import modelo.ContaEspecial;
 import modelo.ContaPoupanca;
@@ -9,6 +10,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Conta conta = null;
+        Cliente cliente = new Cliente("Ana", "111.111.111-11");
         Scanner teclado = new Scanner(System.in);
         System.out.println("---MENU DE OPÇÕES---");
         System.out.println("1- Criar Conta Poupança");
@@ -16,8 +18,8 @@ public class Main {
         System.out.print("Informe o tipo da conta de Ana: ");
         int opcao = teclado.nextInt();
         switch (opcao) {
-            case 1 -> conta = new ContaPoupanca("Ana", 100);
-            case 2 -> conta = new ContaEspecial("Ana", 100, 100);
+            case 1 -> conta = new ContaPoupanca(100, cliente);
+            case 2 -> conta = new ContaEspecial(100, 100, cliente);
             default -> System.out.println("[ERRO] Tipo inválido!");
         }
         conta.imprimeTipoConta("IFPB BANK");
